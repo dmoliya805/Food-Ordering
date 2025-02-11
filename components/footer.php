fd57bd30a819da97d2930e9565a65c9b118e76cc
dmoliya805 <dmoliya805@rku.ac.in>
2025-02-11 13:15:57 +0530

    changes

---
+++ components/footer.php
@@ -0,0 +1,33 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>our email</h3>
+         <a href="mailto:abc345@gmail.com">abc345@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>opening hours</h3>
+         <p>10:00am to 11:10pm</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>our address</h3>
+         <a href="#">India, Rajkot - 360002</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>our number</h3>
+         <a href="tel:1234567890">9656556517</a>
+      </div>
+
+   </section>
+
+   
+</footer>
+
